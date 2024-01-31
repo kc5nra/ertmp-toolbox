e2de234ebde8241042d3f6ee2667271bd1ae4ed7
Ruwen Hahn <haruwenz@twitch.tv>
2024-01-31 19:48:49 +0100

    Remove enums
    Use dynamic dispatch instead

--- src/stream.rs
+++ src/stream.rs
@@ -1,74 +1,53 @@
 use anyhow::Result;
 use bytes::BufMut;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt, ReadHalf, WriteHalf},
-    net::TcpStream,
-};
-use tokio_rustls::client::TlsStream;
+use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
-pub enum StreamReadHalf {
-    Tcp(ReadHalf<TcpStream>),
-    Tls(ReadHalf<TlsStream<TcpStream>>),
-}
+pub struct StreamReadHalf(Box<dyn AsyncRead + Send + Unpin>);
 
 impl StreamReadHalf {
     pub async fn read_buf<'a, B: BufMut>(&'a mut self, buf: &'a mut B) -> Result<usize> {
-        match self {
-            StreamReadHalf::Tcp(s) => Ok(s.read_buf(buf).await?),
-            StreamReadHalf::Tls(s) => Ok(s.read_buf(buf).await?),
-        }
+        Ok(self.0.read_buf(buf).await?)
     }
 }
 
-pub enum StreamWriteHalf {
-    Tcp(WriteHalf<TcpStream>),
-    Tls(WriteHalf<TlsStream<TcpStream>>),
-}
+pub struct StreamWriteHalf(Box<dyn AsyncWrite + Send + Unpin>);
 
 impl StreamWriteHalf {
     pub async fn write_all<'a>(&mut self, src: &'a [u8]) -> Result<()> {
-        match self {
-            StreamWriteHalf::Tcp(s) => Ok(s.write_all(src).await?),
-            StreamWriteHalf::Tls(s) => Ok(s.write_all(src).await?),
-        }
+        Ok(self.0.write_all(src).await?)
     }
 }
-pub enum Stream {
-    Tcp(TcpStream),
-    Tls(TlsStream<TcpStream>),
+
+pub trait Streamable: AsyncRead + AsyncWrite {}
+impl<T: AsyncRead + AsyncWrite> Streamable for T {}
+
+pub struct Stream {
+    read_half: StreamReadHalf,
+    write_half: StreamWriteHalf,
 }
 
 impl Stream {
-    pub async fn split(self) -> (StreamReadHalf, StreamWriteHalf) {
-        match self {
-            Stream::Tcp(s) => {
-                let (read_half, write_half) = tokio::io::split(s);
-                (
-                    StreamReadHalf::Tcp(read_half),
-                    StreamWriteHalf::Tcp(write_half),
-                )
-            }
-            Stream::Tls(s) => {
-                let (read_half, write_half) = tokio::io::split(s);
-                (
-                    StreamReadHalf::Tls(read_half),
-                    StreamWriteHalf::Tls(write_half),
-                )
-            }
+    pub fn new<T: Streamable + Send + Unpin + 'static>(streamable: T) -> Stream {
+        let (read, write) = tokio::io::split(streamable);
+        let (read_half, write_half) = (
+            StreamReadHalf(Box::new(read)),
+            StreamWriteHalf(Box::new(write)),
+        );
+        Stream {
+            read_half,
+            write_half,
         }
     }
 
-    pub async fn read<'a>(&'a mut self, buf: &'a mut [u8]) -> Result<usize> {
-        match self {
-            Stream::Tcp(s) => Ok(s.read(buf).await?),
-            Stream::Tls(s) => Ok(s.read(buf).await?),
-        }
+    pub async fn split(self) -> (StreamReadHalf, StreamWriteHalf) {
+        (self.read_half, self.write_half)
+    }
+
+    pub async fn read<'a, B: BufMut>(&'a mut self, buf: &'a mut B) -> Result<usize> {
+        Ok(self.read_half.read_buf(buf).await?)
     }
 
     pub async fn write_all<'a>(&mut self, src: &'a [u8]) -> Result<()> {
-        match self {
-            Stream::Tcp(s) => Ok(s.write_all(src).await?),
-            Stream::Tls(s) => Ok(s.write_all(src).await?),
-        }
+        Ok(self.write_half.write_all(src).await?)
     }
 }
